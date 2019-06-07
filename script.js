e1d92bcf4f4ef8367f7a2f36ad657f88943ebb8a
olessiap <olessia.potapova@gmail.com>
2019-06-07 16:39:40 -0700

    added api calls to the more ron button

--- script.js
+++ script.js
@@ -6,19 +6,22 @@
 // 3. open the request
 // 4. send it
 
-let xhr = new XMLHttpRequest(); 
+let getRon = function() {
+    let xhr = new XMLHttpRequest(); 
 
-xhr.onreadystatechange = function () {
-    
-    if(xhr.readyState !== 4) return;
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState !== 4) return;
+        
+        if(xhr.status >= 200 && xhr.status < 300) {
+            let data = JSON.parse(xhr.responseText);
+            document.querySelector('blockquote').innerHTML = data;
+        } else {
+            console.log("the request failed");
+        }
+    };
 
-    if(xhr.status >= 200 && xhr.status < 300) {
-        console.log(JSON.parse(xhr.responseText));
-    } else {
-        console.log("the request failed");
-    }
+    xhr.open("GET", "http://ron-swanson-quotes.herokuapp.com/v2/quotes");
+    xhr.send();
+}
 
-};
-
-xhr.open("GET", "http://ron-swanson-quotes.herokuapp.com/v2/quotes");
-xhr.send();
+getRon();
